20c9ad6082fdd8cfebef21af324126666a4357a6
Robson <robsondejesus1996@hotmail.com>
2022-01-23 12:29:30 -0300

    Implementações da opções de usuários onlines

--- project-client/src/telas/OpcoesUsuarios.java
+++ project-client/src/telas/OpcoesUsuarios.java
@@ -5,8 +5,17 @@
  */
 package telas;
 
+import controle.ControleDadosChat;
+import controle.ControleMensagens;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import javax.swing.DefaultListModel;
 import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
 import modelo.Client;
+import org.json.simple.JSONObject;
 
 /**
  *
@@ -20,15 +29,93 @@ public class OpcoesUsuarios extends javax.swing.JFrame {
     
     
     private Client c;
+    private ArrayList<String> usuarios;
+    private ServerSocket serverSocketCliente;
+    private boolean executando;
+
     
-    public OpcoesUsuarios() {
+    
+    
+    public OpcoesUsuarios(Client c) {
+        executando = false;
+        serverSocketCliente = null;
+        this.jList = new JList<>();
+        this.usuarios = new ArrayList<String>();
+        this.c = c;
         initComponents();
+        clientStartSrv(this, Integer.parseInt(c.getPorta()));
+    }
+    
+    
+    
+    private void iniciarChat() {
+        int index = jList.getSelectedIndex();
+        if (index != -1) {
+            String dados = jList.getSelectedValue().toString();
+            String[] inf = dados.split(";");
+            System.out.println(inf[0]);
+            try {
+                Socket socket = new Socket(inf[1], Integer.parseInt(inf[2]));
+                ControleMensagens.dadosEnviados(ControleMensagens.informacoesUsuario(c), socket);
+                ControleDadosChat gc = new ControleDadosChat(socket, this);
+                gc.definirConversa(new Conversa(this, socket, dados));
+                gc.definirConversaRodando(true);
+                new Thread(gc).start();
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "USUÁRIO ESTÁ INDISPONÍVEL NO MOMENTO! ATUALIZE A LISTA");
+                ControleMensagens.dadosEnviados(ControleMensagens.deletarMensagem(inf[2]), c.obterUsuarioSocket());
+                System.err.println("Usuário indisponível: " + e);
+
+            }
+        }
+    }
+    
+    private void clientStartSrv(OpcoesUsuarios menu, int porta){
+        new Thread() {
+            @Override
+            public void run() {
+                executando = true;
+                try {
+                    serverSocketCliente = new ServerSocket(porta);
+                    System.out.println("Server cliente iniciado");
+                    while (executando) {
+                        Socket socket = serverSocketCliente.accept();
+                        ControleDadosChat gc = new ControleDadosChat(socket, menu);
+                        new Thread(gc).start();
+                    }
+                } catch (Exception e) {
+                    System.err.println("Start server client" + e);
+                }
+            }
+        }.start();
+    }
+    
+    
+    private void listaAtualizacao() {
+        ControleMensagens.dadosEnviados(ControleMensagens.listarMensagem(), c.obterUsuarioSocket());
+        String retorno = ControleMensagens.dadosRecebidos(c.obterUsuarioSocket());
+        System.out.println(retorno + "  retorno de usuários!!");
+        c.listagemUsuarios(retorno);
+        usuarios.clear();
+        jList.removeAll();
+
+        DefaultListModel model = new DefaultListModel();
+        JSONObject jobj = new JSONObject();
+        for (Object user : c.getQtdUsuariosOnlines()) {
+            jobj = (JSONObject) user;
+            if (jobj.get("porta").equals(c.getPorta())) {
+            } else {
+                String c = jobj.get("nome") + ";" + jobj.get("enderecoip") + ";" + jobj.get("porta");
+                model.addElement(c);
+            }
+        }
+        jList.setModel(model);
+
     }
 
-    OpcoesUsuarios(Client c) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
+    
+        
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -134,48 +221,24 @@ public class OpcoesUsuarios extends javax.swing.JFrame {
     }
     
     private void btiniciarChatActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btiniciarChatActionPerformed
-        //iniciarChat();
+        iniciarChat();
     }//GEN-LAST:event_btiniciarChatActionPerformed
 
     private void btiniciarChat1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btiniciarChat1ActionPerformed
-        //iniciarChat();
+        iniciarChat();
     }//GEN-LAST:event_btiniciarChat1ActionPerformed
 
     private void btAtualizarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btAtualizarActionPerformed
-        //atualizarLista();
+        listaAtualizacao();
     }//GEN-LAST:event_btAtualizarActionPerformed
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(OpcoesUsuarios.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(OpcoesUsuarios.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(OpcoesUsuarios.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(OpcoesUsuarios.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
+    
+    public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new OpcoesUsuarios().setVisible(true);
             }
         });
     }
